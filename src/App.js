ca5a594bdb2185d9f064c77e1292f64006a74ab9
davideliason <davethemaker@gmail.com>
2017-06-20 06:16:46 -0700

    clear template App structure and styling

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
 class App extends Component {
-  render() {
+  render(){
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2 className="pangolin">Welcome to react_fitness_log</h2>
-        </div>
-        <p className="pangolin">Let us get into shape</p>
-      </div>
-    );
+         <div className = "App">
+            <p>app</p>
+         </div>
+      );
   }
 }
+
+
 
 export default App;
